b493594641054f157a774e2be7e478b3d8d6b3d0
Caleb Ukle <caleb@nrwl.io>
2023-11-30 09:55:27 -0600

    blah

---
+++ apps/ng-demo/src/app/btn-2/btn-2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-cloud-billing-tests-btn-2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './btn-2.component.html',
+  styleUrl: './btn-2.component.css',
+})
+export class Btn2Component {}
